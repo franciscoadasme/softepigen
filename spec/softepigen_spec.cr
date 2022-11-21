980680eec876d9d36f1bb5ddea9a249fb000c6fd
Francisco Adasme <francisco.adasme@gmail.com>
2022-11-21 18:38:18 -0300

    Add spec for main

--- spec/softepigen_spec.cr
+++ spec/softepigen_spec.cr
@@ -1,4 +1,13 @@
 require "./spec_helper"
 
 describe Softepigen do
+  it "reports primers" do
+    expected = File.read "#{__DIR__}/data/chr1777777-out.csv"
+    `shards run --without-development softepigen -- #{__DIR__}/data/chr1777777.fasta`
+    $?.success?.should be_true
+    output = File.read "chr1777777-out.csv"
+    output.should eq expected
+  ensure
+    File.delete "chr1777777-out.csv"
+  end
 end
--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -2,7 +2,7 @@ require "spec"
 require "../src/softepigen"
 
 def test_seq : Softepigen::Region
-  str = File.read("#{__DIR__}/data/test.fasta").split("\n")[1]
+  str = File.read("#{__DIR__}/data/chr1777777.fasta").split("\n")[1]
   str.size.should eq 4162
   Softepigen::Region.new(str)
 end
